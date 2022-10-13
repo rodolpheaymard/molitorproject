9f4f6c70765bf1619e4deb8ff75feafd7da7cbbc
Rodolphe Aymard <rodolpheaa@yahoo.fr>
2022-10-13 11:01:06 +0200

    exhibition player js

---
+++ public/scripts/MgPlayer.js
@@ -0,0 +1,67 @@
+   
+	/* When the openFullscreen() function is executed, open the video in fullscreen.
+	Note that we must include prefixes for different browsers, as they don't support the requestFullscreen method yet */
+	function mgOpenFullscreen() {
+      player = document.getElementById("mgPlayer");
+	  if (player !== null && typeof(player) !== "undefined")
+	  {
+		if (player.requestFullscreen) {
+			player.requestFullscreen();
+		  } else if (player.webkitRequestFullscreen) { /* Safari */
+			player.webkitRequestFullscreen();
+		  } else if (player.msRequestFullscreen) { /* IE11 */
+			player.msRequestFullscreen();
+		  }	
+	  }
+	}
+
+	function mgGoFullscreen() {
+		player = document.getElementById("mgPlayer");
+		if (player !== null && typeof(player) !== "undefined")
+		{
+			player.webkitRequestFullscreen();
+			player.style.display="";
+		}
+		playerOverlay = document.getElementById("mgPlayerOverlay");
+		if (playerOverlay !== null && typeof(playerOverlay) !== "undefined")
+		{
+		    playerOverlay.style.display="none";
+		}
+	}
+	
+	function mgFullscreenChanged() {
+		if (document.webkitFullscreenElement == null) {
+			mf = document.getElementById("main_frame");
+			if (mf !== null && typeof(mf) !== "undefined")
+			{
+				mf.style.display="none";
+			}		
+		}
+	}
+
+	document.onwebkitfullscreenchange = mgFullscreenChanged;
+	document.documentElement.onclick = mgGoFullscreen;
+	document.onkeydown = mgGoFullscreen;
+	var curimg = 0;    
+
+	function mgNextImage() {
+	  var imgfile = "../images/onair/digital_0" + curimg + ".jpg";
+   	  curimg++;
+	  if (curimg>=3)  curimg=0;
+	  return imgfile;
+	}
+	
+	function mgChangeImage() {
+   	    playerImage = document.getElementById("mgPlayerImage");
+		if (playerImage !== null && typeof(playerImage) !== "undefined")
+		{		
+		  playerImage.src = mgNextImage();
+		}
+		setTimeout(mgChangeImage, 2000);
+	}
+	
+	function mgStartPlayer() {
+        setTimeout(mgChangeImage, 1000);
+	}
+	
+    mgStartPlayer();
--- src/components/MgExhibitionPage.js
+++ src/components/MgExhibitionPage.js
@@ -3,10 +3,29 @@ import { Typography } from 'antd';
 //import { Col, Row } from 'antd';
 const { Title } = Typography;
 
+export const appendScript = (scriptToAppend) => {
+  const script = document.createElement("script");
+  script.src = scriptToAppend;
+  script.type = 'text/javascript';
+  script.src = scriptToAppend;  
+  script.async = true;
+  document.body.appendChild(script);
+}
 
 export class MgExhibitionPage extends React.Component {
  
+  componentDidMount () {
+    appendScript(process.env.PUBLIC_URL + '/scripts/MgPlayer.js');
+  }  
+
   render() {
-    return ( <Title>Exhibition</Title> );
+    return ( <>       
+               <div className="MgExhibitionPlayer" id="mgPlayer">     
+               <img className="MgExhibitionPlayerImage"  id="mgPlayerImage" />
+               <div className="MgExhibitionPlayerStart" id="mgPlayerOverlay">
+                press anywhere to go full screen</div> 
+               </div>
+              </>
+  );
   }
 }
